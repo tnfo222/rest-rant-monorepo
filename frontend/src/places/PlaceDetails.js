fbdb5aef6f0324ccbf594a4f519d143788afaad1
tnfo222 <tayforbes92@hotmail.com>
2022-10-04 21:04:24 -0400

    admin only options

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -105,6 +105,21 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className = "btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>
+				<button type="submit" className = "btn btn-danger" onClick = {deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+
 	return (
 		<main>
 			<div className="row">
@@ -131,12 +146,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
